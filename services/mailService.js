cb01af65564b02d442363b3086b30436836af1de
iroro1 <ojigboleo@gmail.com>
2024-08-09 23:46:20 +0100

    email service

--- services/mailService.js
+++ services/mailService.js
@@ -2,11 +2,25 @@ import nodemailer from "nodemailer";
 
 export async function sendMail(subject, fromEmail, otpText) {
   var transporter = nodemailer.createTransport({
-    service: "gmail",
+    port: 465,
+    host: "smtp.gmail.com",
     auth: {
       user: "thos.enterprise12@gmail.com",
       pass: "dzig hwnu iggz jexu",
     },
+    secure: true,
+  });
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
   });
 
   var mailOptions = {
@@ -16,12 +30,23 @@ export async function sendMail(subject, fromEmail, otpText) {
     text: otpText,
   };
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      throw new Error(error);
-    } else {
-      console.log("Email Sent");
-      return true;
-    }
+  // transporter.sendMail(mailOptions, function (error, info) {
+  //   if (error) {
+  //     throw new Error(error);
+  //   } else {
+  //     console.log("Email Sent");
+  //     return true;
+  //   }
+  // });
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log(info);
+        resolve(info);
+      }
+    });
   });
 }
