39c207ea04fbac3159a2ac46a404faca816322cf
iroro1 <ojigboleo@gmail.com>
2024-09-18 16:04:21 +0100

    content

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
-import React from "react";
-import { Button } from "./ui/button";
-import { MoveRight } from "lucide-react";
 import Link from "next/link";
+import { Button } from "./ui/button";
 
 const Hero = () => {
   return (
