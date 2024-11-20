99addf3e3f429c88413be61f6333fa9a6671a88f
Joshua Inyang <joshuainyang255@gmail.com>
2024-11-20 09:33:51 +0100

    google play store button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import Logo from "./Logo";
 import { Button } from "./ui/button";
 import Link from "next/link";
 import { Instagram, Linkedin, Twitter } from "lucide-react";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
@@ -10,28 +11,57 @@ const Navbar = () => {
       <div className="flex w-full items-center justify-between">
         <Logo />
 
-        <div className="flex items-center gap-1 md:gap-3">
-          <Button asChild size="icon" variant="ghost">
+        <div className="flex  items-center sm:gap-1 md:gap-3">
+          <Button
+            asChild
+            size="icon"
+            variant="ghost"
+            className=" hidden sm:flex"
+          >
             <Link
               href="https://www.instagram.com/thehouseofsounds/"
               target="_blank"
             >
-              <Instagram className="size-5 md:size-6" />
+              <Instagram className="size-5 md:size-6 " />
             </Link>
           </Button>
-          <Button asChild size="icon" variant="ghost">
+          <Button
+            asChild
+            size="icon"
+            variant="ghost"
+            className=" hidden sm:flex"
+          >
             <Link href="https://x.com/THOSEAPP" target="_blank">
-              <Twitter className="size-5 md:size-6" />
+              <Twitter className="size-5 md:size-6 " />
             </Link>
           </Button>
-          <Button asChild size="icon" variant="ghost">
+          <Button
+            asChild
+            size="icon"
+            variant="ghost"
+            className="mr-2 hidden sm:flex "
+          >
             <Link
               href="https://www.linkedin.com/company/the-house-of-sounds/"
               target="_blank"
             >
-              <Linkedin className="size-5 md:size-6" />
+              <Linkedin className="size-5 md:size-6 " />
             </Link>
           </Button>
+
+          {/* DOWNLOAD ON PLAY STORE */}
+          <Link
+            href="https://play.google.com/store/apps/details?id=com.thehouseofsounds.yarnapp"
+            target="_blank"
+          >
+            <Image
+              className="object-right "
+              width={120}
+              height={120}
+              alt="google-play"
+              src={`/assets/images/download-button/play-store.png`}
+            />
+          </Link>
         </div>
       </div>
     </div>
